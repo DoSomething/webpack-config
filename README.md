bfd85cfe6b6b6e03e928532c7119553b5088ad18
David Furnes <david@dfurnes.com>
2016-03-21 11:29:02 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-DoSomething.org ESLint config
-=============================
+# webpack-config
 
-This is our shared [ESLint](http://eslint.org) config used for JavaScript projects at DoSomething.org. It can be used by setting the `extends` property in your `.eslintrc`:
+This is our shared [Webpack](http://webpack.github.io) config used for front-end projects at DoSomething.org. It builds JavaScript with [Babel](https://babeljs.io), SCSS with [LibSass](http://sass-lang.com/libsass), and CSS with [Autoprefixer](https://github.com/postcss/autoprefixer) and [CSS-MQPacker](https://github.com/hail2u/node-css-mqpacker). It is also configured to hash static asset filenames, or bundle as Data URIs if small enough.
 
-```json
-{
-      "extends": "@dosomething/eslint-config"
-}
+### Getting Started
+
+```js
+var webpack = require('webpack');
+var config = require('@dosomething/webpack-config');
+
+module.exports = config({
+  entry: {
+    'main': './src/index.js'
+  }
+});
 ```
+
+### License
+&copy;2016 DoSomething.org. @dosomething/webpack-config is free software, and may be redistributed under the
+terms specified in the [LICENSE](https://github.com/DoSomething/webpack-config/blob/master/LICENSE) file. The
+name and logo for DoSomething.org are trademarks of Do Something, Inc and may not be used without permission.
--- index.js
+++ index.js
@@ -1,18 +1,105 @@
 /**
- * This is DoSomething.org's shared ESLint config
- * for enforcing consistent code style in our JavaScript
- * projects. It's based on Airbnb's JavaScript style guide.
- *
- * @see https://github.com/DoSomething/code-style/tree/master/javascript
- * @see https://github.com/airbnb/javascript
+ * This is DoSomething.org's shared Webpack config for
+ * building JavaScript, SCSS, and other front-end assets.
  */
 
-module.exports = {
-  "extends": "airbnb",
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var defaults = require('lodash/defaultsDeep');
+var assign = require('lodash/assign');
+var path = require('path');
+var fs = require('fs');
 
-  // We make a few tweaks to the stock rules:
-  "rules": {
-    "func-names": 0,
-    "id-length": [2, { "exceptions": ["i", "j", "k", "$"] }],
-  },
+var config = {
+    entry: {
+      // ...  
+    },
+    output: {
+        filename: '[name].js',
+        path: 'dist',
+        libraryTarget: 'umd',
+    },
+    externals: {
+        // Don't bundle the 'jquery' package with the library (forge.js), but
+        // instead load from `jQuery` global variable or AMD/CJS package.
+        'jquery': {
+            root: 'jQuery',
+            commonjs2: 'jquery',
+            amd: 'jquery'
+        }
+    },
+    module: {
+        loaders: [
+            // Bundle JavaScript, and transform to ES5 using Babel.
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+
+            // Bundle static assets, either hashing filename or inlining into bundle if under 8KB
+            { test: /\.(png|jpg|eot|gif|woff|svg|ttf)$/, loader: 'url?limit=8192' },
+
+            // Bundle CSS stylesheets and process with PostCSS, extract to single CSS file per bundle.
+            { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap!postcss') },
+
+            // Bundle SCSS stylesheets (processed with LibSass & PostCSS), extract to single CSS file per bundle.
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('css?sourceMap!postcss!sass?sourceMap') }
+        ]
+    },
+    postcss: function() {
+        return [
+            // Automatically add vendor prefixes using Autoprefixer.
+            require('autoprefixer')({
+                browsers: ['last 4 versions', 'Firefox ESR', 'Opera 12.1'],
+            }),
+
+            // Combine media queries using CSS-MQPacker.
+            require('css-mqpacker')()
+        ];
+    },
+    plugins: [
+        // Make NODE_ENV accessible from within client scripts (for conditional dev/prod builds).
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': process.env.NODE_ENV
+            }
+        }),
+
+        // Extract all stylesheets referenced in each bundle into a single CSS file.
+        new ExtractTextPlugin('[name].css'),
+        // ...
+    ]
+};
+
+if(process.env.NODE_ENV === 'production') {
+    // In production, minify our output with UglifyJS
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+          compress: {
+              warnings: false,
+              drop_console: true,
+              drop_debugger: true,
+              dead_code: true
+          }
+      })
+    )
+} else {
+    // Enable inline source maps when in development.
+    config.devtool = '#inline-source-map';
+
+    // Instruct Webpack to watch for changes & rebuild.
+    config.watch = true;
 }
+
+var configurator = function(options) {
+    var c = defaults(config, options);
+
+    // If we're running in NPM 2.x, we need to tell Webpack to check
+    // this package's `node_modules` when resolving loaders
+    var packageModules = path.join(__dirname, 'node_modules');
+    if (fs.existsSync(packageModules)) {
+        assign(config, { resolveLoader: { modulesDirectories: [packageModules] } });
+    }
+    
+    return c;
+};
+
+module.exports = configurator;
+
